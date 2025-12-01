72fbb0af292ec872e78aafd39a71634d99267d47
ege <egehamzay55@gmail.com>
2025-12-01 23:09:44 +0300

    Update port_scanner.py

--- networking/port_scanner.py
+++ networking/port_scanner.py
@@ -1,1 +1,34 @@
+import socket
+import threading
+
+open_ports = []
+
+def scan_port(ip, port):
+    try:
+        s = socket.socket()
+        s.settimeout(0.5)
+        s.connect((ip, port))
+        open_ports.append(port)
+        s.close()
+    except:
+        pass
+
+def scan(ip):
+    threads = []
+    print(f"Scanning {ip}...")
+    
+    for port in range(1, 65536):
+        t = threading.Thread(target=scan_port, args=(ip, port))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print("Open ports:")
+    print(open_ports)
+
+if __name__ == "__main__":
+    target = input("Target IP: ")
+    scan(target)
 
